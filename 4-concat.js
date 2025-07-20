ef8dc0f30624f46868f5a3b0bc2582135c2c93e3
Bruno <brunokayitare85@gmail.com>
2025-07-20 23:52:38 +0200

    Complete Task 4: Argument concatenation

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,3 @@
 const [first, second] = process.argv.slice(2);
-console.log(\`\${first || "undefined"} is \${second || "undefined"}\`);
+console.log(`${first || "undefined"} is ${second || "undefined"}`);
 
